14998f52510303c91c2c16065a0538480437a4da
Lejian_Zhu <119600276+LiebeDieb@users.noreply.github.com>
2023-11-12 22:03:14 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,9 +5,4 @@ setup(
     version='1.0',
     author='Lejian Zhu',
     description='solution for dsss homework 2',
-    packages=['your_package'],
-    install_requires=[
-        'dependency1',
-        'dependency2',
-    ],
 )
